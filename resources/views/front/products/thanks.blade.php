c1de2f9b45f27c275c66f07135fe3f6bea336961
Matias Gonzalez <matsalez@gmail.com>
2019-08-05 18:23:29 -0300

    Modificaciones visuales vista thanks

--- resources/views/front/products/thanks.blade.php
+++ resources/views/front/products/thanks.blade.php
@@ -15,12 +15,15 @@
     </div>
     <div class="col-12 col-md-3">
       <section class="detalle-producto">
-        <p class="gracias"> ¡Gracias por su compra!</p>
-        <h3 class="titulo-producto">{{ $theProduct->name }}</h3>
+        <p class="gracias"> ¡Gracias por su compra {{ Auth::user()->name }} !</p>
+        <h3 class="titulo-producto"> Usted compro: {{ $theProduct->name }}</h3>
         <hr>
-        <p class="precio-producto-compra">{{ $theProduct->price }}</p>
-        <p class="descripcion">{{ $theProduct->description }}</p>
-        <p class="descripcion">{{ $theProduct->category->name }}</p>
+        <p class="precio-producto-compra">Costo: {{ $theProduct->price }}</p>
+        <p class="descripcion">Descripción: {{ $theProduct->description }}</p>
+        <p class="descripcion">Cateogoria: {{ $theProduct->category->name }}</p>
+        <p class="descripcion">  Te envíamos un correo a <b> {{ Auth::user()->email }} </b> con información sobre tu compra.</p>
+        <hr>
+        <p class="descripcion">Para conocer el estado de su envío haga clic <a href="http://www.sairesenvios.com.ar/tracking/?c=QZA871"><b> AQUI </b></a> </p>
         <br>
   </div>
     </div>
